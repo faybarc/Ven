4899320e2041a7ff799a7867101b6a0ec1fca887
Francisco Aybar <f.aybarcabrera13@gmail.com>
2026-03-10 07:38:22 -0400

    Armado de los proyectos, creacion del DataContext, Primer Modelo Country. Todo funcionando con la BD y sus comandos, Add-migration y el Update-Database.

---
+++ Ven/Van.Shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Van.Shared.Entities;
+
+public class Country
+{
+    [Key]
+    public int IdCountry { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string CountryName { get; set; } = null!;
+}
