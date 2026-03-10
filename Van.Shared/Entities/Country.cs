9a4417ac433482925d8ede784c36a3986e088a23
Francisco Aybar <f.aybarcabrera13@gmail.com>
2026-03-10 07:59:31 -0400

    Arregle una carpeta

---
+++ Van.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Van.Shared.Entities;
+
+public class Country
+{
+    [Key]
+    public int IdCountry { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string CountryName { get; set; } = null!;
+
+}
